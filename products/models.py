b2bf273fe98fc1ec3387ddfeca4a517d3e977be8
jenns93 <jjenns93@gmail.com>
2022-05-30 13:10:26 +0000

    Added description and image to products and models

--- products/models.py
+++ products/models.py
@@ -22,6 +22,8 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=50)
     style = models.CharField(max_length=50)
+    description = models.CharField(max_length=254, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     overall = models.CharField(max_length=50, null=True, blank=True)
     
