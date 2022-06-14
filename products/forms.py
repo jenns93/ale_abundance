414d1887e43fe59e71c79a4ad07c670f8a08800c
jenns93 <jjenns93@gmail.com>
2022-06-14 19:35:11 +0000

    Added product view, url, andtemplate  forpPoduct adminn

--- products/forms.py
+++ products/forms.py
@@ -6,13 +6,18 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ('sku','name','ABV', 'style',
+                  'category','description', 
+                  'image', 'overall', 'price',
+                  )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
+        self.fields['sku'].widget.attrs['autofocus'] = True
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-10'
+   
