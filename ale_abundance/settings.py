5ca2417e43fa0aa9094a2c1b7b64af6f9bb0c860
jenns93 <jjenns93@gmail.com>
2022-06-15 01:36:48 +0000

    Removed secretkey and set debug

--- ale_abundance/settings.py
+++ ale_abundance/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vp3%&wlb^634#n8i1gsgk6v2c=rrbzn2ty%@e@i^91vb4l%w9l'
+os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ale-abundance.herokuapp.com', 'localhost']
 
