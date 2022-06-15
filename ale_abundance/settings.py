6191d02302a8954523d77cd0ac12f413bf39d2a8
jenns93 <jjenns93@gmail.com>
2022-06-15 01:22:30 +0000

    Heroku deployment

--- ale_abundance/settings.py
+++ ale_abundance/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-vp3%&wlb^634#n8i1gsgk6v2c=rrbzn2ty%@e@i^91vb4l%w9l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ale-abundance.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,12 +119,17 @@ WSGI_APPLICATION = 'ale_abundance.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
